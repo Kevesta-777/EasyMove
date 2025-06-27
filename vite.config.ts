c5e587eb4cd4e1490307daf585dc89035af9b3f8
EliteSniper-1201 <elitesniper@gmail.com>
2025-06-28 00:48:47 +0900

    change the upload size

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
+const isReplit = process.env.REPL_ID !== undefined;
+
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
+    ...(isReplit ? [runtimeErrorOverlay()] : []),
+    ...(process.env.NODE_ENV !== "production" && isReplit
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
             m.cartographer(),
